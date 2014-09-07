cd22a8e3132d43e00ae32d4211fc58039e867b3e
yuhattori <yuhattori14@gmail.com>
2014-09-08 00:31:42 +0900

    getterとsetterの設定

--- src/com/illustratedbooks/story/StoryDisplayData.java
+++ src/com/illustratedbooks/story/StoryDisplayData.java
@@ -17,16 +17,16 @@ public class StoryDisplayData {
 	private int mDp_h = 0;// ディスプレイの高さ
 
 	// Background
-	private int mDrowBG_w = 0;// 背景を実際に表示するときの画像の幅
-	private int mDrowBG_h = 0;// 背景を実際に表示するときの画像の高さ
-	private int mDrowPosBG_x = 0;// 背景のx軸の表示位置
-	private int mDrowPosBG_y = 0;// 背景のy軸の表示位置
+	private int mDrownBG_w = 0;// 背景を実際に表示するときの画像の幅
+	private int mDrownBG_h = 0;// 背景を実際に表示するときの画像の高さ
+	private int mDrownPosBG_x = 0;// 背景のx軸の表示位置
+	private int mDrownPosBG_y = 0;// 背景のy軸の表示位置
 
 	// MessageWing
-	private int mDrowMSG_w = 0;// 背景を実際に表示するときの画像の幅
-	private int mDrowMSG_h = 0;// 背景を実際に表示するときの画像の高さ
-	private int mDrowPosMSG_x = 0;// 背景のx軸の表示位置
-	private int mDrowPosMSG_y = 0;// 背景のy軸の表示位置
+	private int mDrownMSG_w = 0;// 背景を実際に表示するときの画像の幅
+	private int mDrownMSG_h = 0;// 背景を実際に表示するときの画像の高さ
+	private int mDrownPosMSG_x = 0;// 背景のx軸の表示位置
+	private int mDrownPosMSG_y = 0;// 背景のy軸の表示位置
 	private int mPaddingMSG = 0;
 
 	/**
@@ -74,22 +74,22 @@ public class StoryDisplayData {
 		// 背景画像のサイズを画面サイズに初期化
 		if (bmp.getHeight() >= bmp.getWidth()) {
 			/* 縦長の画像の場合 */
-			mDrowBG_w = mDp_h * (bmp.getWidth() / bmp.getHeight()); // 背景画像の幅
-			mDrowBG_h = mDp_h;// 背景画像の高さ
-			mDrowPosBG_x = (mDp_w - mDrowBG_w) / 2; // 描画始点のx座標
-			mDrowPosBG_y = 0;// 描画始点のy座標
+			mDrownBG_w = mDp_h * (bmp.getWidth() / bmp.getHeight()); // 背景画像の幅
+			mDrownBG_h = mDp_h;// 背景画像の高さ
+			mDrownPosBG_x = (mDp_w - mDrownBG_w) / 2; // 描画始点のx座標
+			mDrownPosBG_y = 0;// 描画始点のy座標
 		} else {
 			// 横長の画像の場合
-			mDrowBG_w = mDp_w;// 背景画像の幅
-			mDrowBG_h = mDp_w * (bmp.getHeight() / bmp.getWidth()); // 背景画像の高さ
-			mDrowPosBG_x = 0;// / 描画始点のx座標
-			mDrowPosBG_y = (mDp_h - mDrowBG_h) / 2;// 描画始点のy座標
+			mDrownBG_w = mDp_w;// 背景画像の幅
+			mDrownBG_h = mDp_w * (bmp.getHeight() / bmp.getWidth()); // 背景画像の高さ
+			mDrownPosBG_x = 0;// / 描画始点のx座標
+			mDrownPosBG_y = (mDp_h - mDrownBG_h) / 2;// 描画始点のy座標
 		}
 		/* MessageWindow */
-		mDrowMSG_w = mDp_w / 2;// メッセージウィンドウの幅を設定
-		mDrowMSG_h = mDp_h / 5;// メッセージウィンドウの高さを設定
-		mDrowPosMSG_x = (mDp_w - mDrowMSG_w) / 2;// 画面に対して中央に来るように設定
-		mDrowPosMSG_y = (mDp_h - mDrowMSG_h) - mPaddingMSG;// 画面下からpadding分空いた位置に設定
+		mDrownMSG_w = mDp_w / 2;// メッセージウィンドウの幅を設定
+		mDrownMSG_h = mDp_h / 5;// メッセージウィンドウの高さを設定
+		mDrownPosMSG_x = (mDp_w - mDrownMSG_w) / 2;// 画面に対して中央に来るように設定
+		mDrownPosMSG_y = (mDp_h - mDrownMSG_h) - mPaddingMSG;// 画面下からpadding分空いた位置に設定
 	}
 
 	/**
@@ -103,23 +103,23 @@ public class StoryDisplayData {
 		/* BackGroud */
 		if (bmp.getHeight() >= bmp.getWidth()) {
 			// 縦長の画像の場合
-			mDrowBG_w = mDp_h * (bmp.getWidth() / bmp.getHeight());// 背景画像の幅
-			mDrowBG_h = mDp_h;// 背景画像の高さ
-			mDrowPosBG_x = (int) (mDp_w * perMwdInDisplay)
-					+ ((mDp_w - mDrowMSG_w) / 2);// 描画始点のx座標
-			mDrowPosBG_y = 0;// 描画始点のy座標
+			mDrownBG_w = mDp_h * (bmp.getWidth() / bmp.getHeight());// 背景画像の幅
+			mDrownBG_h = mDp_h;// 背景画像の高さ
+			mDrownPosBG_x = (int) (mDp_w * perMwdInDisplay)
+					+ ((mDp_w - mDrownMSG_w) / 2);// 描画始点のx座標
+			mDrownPosBG_y = 0;// 描画始点のy座標
 		} else {
 			// 横長の画像の場合
-			mDrowBG_w = mDp_w;// 背景画像の幅
-			mDrowBG_h = mDp_w * (bmp.getHeight() / bmp.getWidth()); // 背景画像の高さ
-			mDrowPosBG_x = (int) (mDp_w * perMwdInDisplay);// / 描画始点のx座標
-			mDrowPosBG_y = (mDp_h - mDrowBG_h) / 2;// 描画始点のy座標
+			mDrownBG_w = mDp_w;// 背景画像の幅
+			mDrownBG_h = mDp_w * (bmp.getHeight() / bmp.getWidth()); // 背景画像の高さ
+			mDrownPosBG_x = (int) (mDp_w * perMwdInDisplay);// / 描画始点のx座標
+			mDrownPosBG_y = (mDp_h - mDrownBG_h) / 2;// 描画始点のy座標
 		}
 		/* MessageWindow */
-		mDrowMSG_w = (int) (mDp_w * perMwdInDisplay);// メッセージウィンドウの幅を設定
-		mDrowMSG_h = mDp_h;// メッセージウィンドウの高さを設定
-		mDrowPosMSG_x = 0;
-		mDrowPosMSG_y = 0;
+		mDrownMSG_w = (int) (mDp_w * perMwdInDisplay);// メッセージウィンドウの幅を設定
+		mDrownMSG_h = mDp_h;// メッセージウィンドウの高さを設定
+		mDrownPosMSG_x = 0;
+		mDrownPosMSG_y = 0;
 	}
 
 	public void setDisplayData(Context context) {
@@ -139,5 +139,86 @@ public class StoryDisplayData {
 	 */
 	public void setDefaultPaddingMSG() {
 		this.mPaddingMSG = mDp_w / 50;
+	}
+
+	/* setter and getter */
+	public int getLayoutMode() {
+		return mLayoutMode;
+	}
+
+	public void setLayoutMode(int mLayoutMode) {
+		this.mLayoutMode = mLayoutMode;
+	}
+
+	public int getDrownBGWidth() {
+		return mDrownBG_w;
+	}
+
+	public void setDrownBGWidth(int mDrownBG_w) {
+		this.mDrownBG_w = mDrownBG_w;
+	}
+
+	public int getDrownBGHeight() {
+		return mDrownBG_h;
+	}
+
+	public void setDrownBGHeight(int mDrownBG_h) {
+		this.mDrownBG_h = mDrownBG_h;
+	}
+
+	public int getDrownPosBG_x() {
+		return mDrownPosBG_x;
+	}
+
+	public void setDrownPosBG_x(int mDrownPosBG_x) {
+		this.mDrownPosBG_x = mDrownPosBG_x;
+	}
+
+	public int getDrownPosBG_y() {
+		return mDrownPosBG_y;
+	}
+
+	public void setDrownPosBG_y(int mDrownPosBG_y) {
+		this.mDrownPosBG_y = mDrownPosBG_y;
+	}
+
+	public int getDrownMSGWidth() {
+		return mDrownMSG_w;
+	}
+
+	public void setDrownMSGWidth(int mDrownMSG_w) {
+		this.mDrownMSG_w = mDrownMSG_w;
+	}
+
+	public int getDrownMSGHeight() {
+		return mDrownMSG_h;
+	}
+
+	public void setDrownMSGHeight(int mDrownMSG_h) {
+		this.mDrownMSG_h = mDrownMSG_h;
+	}
+
+	public int getDrownPosMSG_x() {
+		return mDrownPosMSG_x;
+	}
+
+	public void setDrownPosMSG_x(int mDrownPosMSG_x) {
+		this.mDrownPosMSG_x = mDrownPosMSG_x;
+	}
+
+	public int getDrownPosMSG_y() {
+		return mDrownPosMSG_y;
+	}
+
+	public void setDrownPosMSG_y(int mDrownPosMSG_y) {
+		this.mDrownPosMSG_y = mDrownPosMSG_y;
+	}
+
+	public int getPaddingMSG() {
+		return mPaddingMSG;
+	}
+
+	public void setPaddingMSG(int mPaddingMSG) {
+		this.mPaddingMSG = mPaddingMSG;
 	}
 }
