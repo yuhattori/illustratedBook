fa301b6640fd7d7f73760cca0f49ba7b5e96e5be
yuhattori <yuhattori14@gmail.com>
2014-09-11 07:48:57 +0900

    MessegeSurfaceView.classを用意

--- src/com/illustratedbooks/fragment/StoryMessegeWindowFragment.java
+++ src/com/illustratedbooks/fragment/StoryMessegeWindowFragment.java
@@ -2,9 +2,12 @@ package com.illustratedbooks.fragment;
 
 import com.illustratedbooks.R;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -16,11 +19,41 @@ public class StoryMessegeWindowFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		mView =inflater.inflate(R.layout.fragment_story_messege_window, container, false);
+		MessegeSurfaceView sf = new MessegeSurfaceView(getActivity());
 		return mView;
 	}
 
 	@Override
 	public void onStart() {
 		super.onStart();
+	}
+	
+	private class MessegeSurfaceView extends SurfaceView implements
+	SurfaceHolder.Callback{
+
+		public MessegeSurfaceView(Context context) {
+			super(context);
+			// TODO 自動生成されたコンストラクター・スタブ
+		}
+
+		@Override
+		public void surfaceCreated(SurfaceHolder holder) {
+			// TODO 自動生成されたメソッド・スタブ
+			
+		}
+
+		@Override
+		public void surfaceChanged(SurfaceHolder holder, int format, int width,
+				int height) {
+			// TODO 自動生成されたメソッド・スタブ
+			
+		}
+
+		@Override
+		public void surfaceDestroyed(SurfaceHolder holder) {
+			// TODO 自動生成されたメソッド・スタブ
+			
+		}
+		
 	}
 }
