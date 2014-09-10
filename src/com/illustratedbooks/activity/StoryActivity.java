36065d3934e9e8acac07291c24058228bce9d9c4
yuhattori <yuhattori14@gmail.com>
2014-09-11 07:18:45 +0900

    背景が表示できるように実装

--- src/com/illustratedbooks/activity/StoryActivity.java
+++ src/com/illustratedbooks/activity/StoryActivity.java
@@ -1,22 +1,42 @@
 package com.illustratedbooks.activity;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.concurrent.ScheduledExecutorService;
+
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.text.style.BackgroundColorSpan;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.SurfaceHolder;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.illustratedbooks.R;
 import com.illustratedbooks.fragment.StoryBackgroundFragment;
 import com.illustratedbooks.fragment.StoryMessegeWindowFragment;
 import com.illustratedbooks.fragment.TopFragment;
+import com.illustratedbooks.story.StoryDisplayData;
 import com.illustratedbooks.story.StorySurfaceView;
 
 public class StoryActivity extends FragmentActivity {
@@ -25,41 +45,160 @@ public class StoryActivity extends FragmentActivity {
 	private static final String BACKGROUND_FLAGMENT = "background_frangment";
 	private static final String MESSEGE_WINDOW_FLAGMENT = "messege_window_frangment";
 
+	private Activity mAct;
+
 	private LinearLayout mBgLayout;
 	private LinearLayout mMsgLayout;
 
-	private Activity mAct;
+	/* 描写関連 */
+	private ScheduledExecutorService mDrowTask;// 表示用スレッド
+	private ScheduledExecutorService mAutoModeTask;// オートモード用スレッド
+	private Boolean mAutoModeFlag = false;// オートモード用のフラグ
+	private int mAutoModeSp = 1000;// オートモードのスピード
+	private static int MSG_ALL = -1;// 文字送りをせずすべてを表示させるときに使用
+	public static final Boolean ON = true;// オートモードON
+	public static final Boolean OFF = false;// オートモードOFF
+	private int mMsgSpd = 100;// 文字送りする速度(=画面更新速度)(ms)
+	private static final int FASTEST_MSG_SPEED = 10;// 文字送りする速度=画面の更新速度の最速値
+	private int mNowPrintMsgNum = 0;// 現在表示しているメッセージの文字数
+
+	// 背景
+	private String mBgPath; // 背景画像のパス
+	private Bitmap mBgFig;// 背景画像
+
+	// テキスト
+	private Paint mPaintf; // テキストのプロパティ
+	private int FONT_SIZE = 32;// フォントのサイズ
+	// メッセージウィンドウ
+	public final static String MSGWIN_PATH = "window.jpg";// ウィンドウのレイアウト
+	private Bitmap mMsgWin;// ウィンドウ画像
+	private Paint mPaintw; // メッセージウィンドウのプロパティ
+	private final int ALPHA = 140;// 透過度
+
+	/* CSVファイル関連 */
+
+	public final static String CSV_FILE_NAME = "scenarios.csv";// CSVファイル名
+	private ArrayList<String[]> mCSVdata = new ArrayList<String[]>(); // CSVファイルの二次元データ
+	private int mCSVColumnNo = 1;// CSVファイルにはヘッダーがあるため0行目は飛ばす
+	public final int ROW = 0;
+	public final int BACK_GROUND = 1;
+	public final int BGM = 2;
+	public final int SE = 3;
+	public final int TEXT = 4;
+
+	/* タッチイベント関係 */
+	final int SINGLE_TAP = 1;
+	final int LONG_TAP = 2;
+	final int DOUBLE_TAP = 3;// 未実装
+	final int FLICK_RIGHT = 4;
+	final int FLICK_LEFT = 5;
+	final int FLICK_UP = 6;
+	final int FLICK_DOWN = 7;
+
+	private float mBefore_x;// MOVE後のUPでｘ座標と比較する
+	private float mBefore_y;// MOVE後のUPでｙ座標と比較する
+	private long mTouchDownTimeMillis;// 画面にタッチした瞬間の時間
+	private Boolean mWindowFlag = true;// ロングタップ時のウィンドウを消すかどうかの判断
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.mAct = this;
-		setContentView(R.layout.activity_story);
+		init();// 初期化
+		setContentView(R.layout.activity_story);// ビューを設定
+
 		Intent intent = getIntent();
 		String clickedBtn = intent.getStringExtra("clickedBtn");
 		if (clickedBtn.equalsIgnoreCase("startBtn")) {
 			// はじめからボタンを押してStoryActivityに来たとき
 		} else {
 			// つづきからボタンを押してStoryActivityに来たとき
+			/* 保存データを読み込む。前回の表示していたROWの読み込み */
+			SharedPreferences sPref = mAct.getSharedPreferences("savedata"
+					+ CSV_FILE_NAME, Context.MODE_PRIVATE);
+			mCSVColumnNo = Integer.valueOf(sPref.getString("savedRowNo", "1"));
+			Log.d(TAG, "RowNo of Saved data is " + mCSVColumnNo);
 		}
 
 	}
 
+	/**
+	 * 共通の初期化処理
+	 */
+	private void init() {
+		/* 設定データの読み込み */
+		SharedPreferences cPref = mAct.getSharedPreferences("configdata", 0);
+		mMsgSpd = (cPref.getInt("readingSpd", 100));// メッセージスピードの設定
+		if (mMsgSpd < FASTEST_MSG_SPEED)
+			mMsgSpd = FASTEST_MSG_SPEED;// 最速表示の場合
+
+		/* CSVファイルの情報の読み込み */
+		try {
+			readCSV();
+		} catch (IOException e) {
+			// TODO CSVファイル読み込みの失敗のポップアップを表示する
+			Log.e(TAG, "failed reading CSVfile");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * assetsフォルダ直下におかれたCSVファイルの読み込みを行う
+	 * 
+	 * @throws IOException
+	 * @throws UnsupportedEncodingException
+	 */
+	private void readCSV() throws IOException, UnsupportedEncodingException {
+		AssetManager assets = getResources().getAssets();
+		InputStream in = assets.open(CSV_FILE_NAME);
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(in, "SJIS"));// S-JISに変換
+		String str;
+
+		while ((str = br.readLine()) != null) {
+			String[] array = str.split(",");
+			mCSVdata.add(array);
+		}
+	}
+
 	@Override
 	public void onWindowFocusChanged(boolean hasFocus) {
+		/* 背景画像を取得 */
+		try {
+			// 背景の画像のPATHを取得
+			mBgPath = "background/" + mCSVdata.get(mCSVColumnNo)[BACK_GROUND];
+			// 背景画像を取得
+			mBgFig = BitmapFactory.decodeStream(getResources().getAssets()
+					.open(mBgPath));
+		} catch (IOException e) {
+			Log.e(TAG, "failed reading background  image file");
+			Toast.makeText(mAct, "failed reading background image file",
+					Toast.LENGTH_SHORT).show();
+			e.printStackTrace();
+		}
+
+		/* onCreateではレンダリングが終わっていないためここで行う */
 		// フラグメントを入れるレイアウトを取得
-		mBgLayout = (LinearLayout) mAct
-				.findViewById(R.id.background_layout);
+		mBgLayout = (LinearLayout) mAct.findViewById(R.id.background_layout);
 		mMsgLayout = (LinearLayout) mAct
 				.findViewById(R.id.messege_window_layout);
-
-		/* onCreateではレンダリングが終わっていないため */
 		// Fragmentを管理するFragmentManagerを取得
 		FragmentManager manager = getSupportFragmentManager();
 		// 追加や削除などを1つの処理としてまとめるためのトランザクションクラスを取得
 		FragmentTransaction ft = manager.beginTransaction();
 
+		/* StoryBackgroundFragmentを設定 */
 		StoryBackgroundFragment bgf = new StoryBackgroundFragment();
+		// 背景画像を登録
+		Bundle args = new Bundle();  
+		args.putParcelable("BgFig", mBgFig);
+		args.putFloat("pointX", mBgLayout.getX());//backgroundのX座標
+		args.putFloat("pointY", mBgLayout.getY());//backgroundのY座標
+		args.putInt("width", mBgLayout.getWidth());//レイアウトの幅
+		args.putInt("height", mBgLayout.getHeight());//レイアウトの高さ
+		bgf.setArguments(args);
+
+		/* StoryBackgroundFragmentを設定 */
 		StoryMessegeWindowFragment mwf = new StoryMessegeWindowFragment();
 
 		// Fragment をスタックに追加する
@@ -68,27 +207,52 @@ public class StoryActivity extends FragmentActivity {
 		ft.commit();
 	}
 
-	public void addFragmentToStack() {
-	    // フラグメントのインスタンスを生成する。
-		StoryBackgroundFragment newFragment = new StoryBackgroundFragment();
-		
+	/**
+	 * フラグメントを変更する
+	 * 
+	 * @param fragment
+	 *            スタックへ登録するフラグメント
+	 */
+	public void replaceFragmentToStack(Fragment fragment) {
+		// フラグメントのクラス名を取得
+		String fragmentName = fragment.getClass().getSimpleName();
+
 		// Fragmentを管理するFragmentManagerを取得
 		FragmentManager manager = getSupportFragmentManager();
-		// 追加や削除などを1つの処理としてまとめるためのトランザクションクラスを取得(新しく作らないと”commit already called”とエラーが出る)
+		// 追加や削除などを1つの処理としてまとめるためのトランザクションクラスを取得(新しく作らないと”commit already
+		// called”とエラーが出る)
 		FragmentTransaction ft = manager.beginTransaction();
-	    // Layout位置先の指定
-		ft.replace(mMsgLayout.getId(), newFragment);
-	    // Fragmentの変化時のアニメーションを指定
+
+		if (fragmentName.equals("StoryBackgroundFragment")) {
+			/* StoryBackgroundFragmentの場合 */
+			// Layout位置先の指定
+			ft.replace(mBgLayout.getId(), fragment);
+		} else if (fragmentName.equals("StoryMessegeWindowFragment")) {
+			/* StoryMessegeWindowFragmentの場合 */
+			// Layout位置先の指定
+			ft.replace(mMsgLayout.getId(), fragment);
+		} else {
+			// それ以外のフラグメントの時
+			Log.e(TAG, "not exist " + fragmentName);
+			Toast.makeText(mAct, "エラーが発生しました", Toast.LENGTH_SHORT).show();
+			ft.commit();
+			return;
+		}
+		// Fragmentの変化時のアニメーションを指定
 		ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
 		ft.addToBackStack(null);
 		ft.commit();
 	}
-	
+
+	public void replaceBackground(Bitmap bmp) {
+		StoryBackgroundFragment bgf = new StoryBackgroundFragment();
+		replaceFragmentToStack(bgf);
+	}
+
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		switch (keyCode) {
 		case KeyEvent.KEYCODE_BACK:
-			addFragmentToStack();
 			// // バックボタンが押されたとき
 			// if(mStSf.isAutoMode())
 			// //オートモード中の場合は解除
@@ -126,5 +290,16 @@ public class StoryActivity extends FragmentActivity {
 		default:
 			return false;
 		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// 画面が閉じる時にデータを保存
+		SharedPreferences pref = mAct.getSharedPreferences("savedata"
+				+ CSV_FILE_NAME, Context.MODE_PRIVATE);
+		Editor editor = pref.edit();
+		editor.putString("savedRowNo", mCSVdata.get(mCSVColumnNo)[ROW]);
+		editor.commit();
 	}
 }
--- src/com/illustratedbooks/fragment/StoryBackgroundFragment.java
+++ src/com/illustratedbooks/fragment/StoryBackgroundFragment.java
@@ -11,39 +11,63 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class StoryBackgroundFragment extends Fragment {
 	private View mView;
-	private Bitmap mBitmap;
-	private String mm ="反映されていません";
+	/*レイアウトの情報*/
+	private float mPointX;//レイアウトのx座標
+	private float mPointY;//レイアウトのy座標
+	private int mWidth;//レイアウトの幅
+	private int mHeight;//レイアウトの高さ
 	
+	/*画像ファイル*/
+	private Bitmap mBgFig;//画像ファイル
+	private int mDrownBG_w;
+	private int mDrownBG_h;
+	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		mView = inflater.inflate(R.layout.fragment_story_background, container, false);
+		mView = inflater.inflate(R.layout.fragment_story_background, container,
+				false);
+		//Bundleから情報を取得する
+		mBgFig=getArguments().getParcelable("BgFig");//画像ファイルを設定
+		mPointX=getArguments().getFloat("pointX");//レイアウトのx座標の設定
+		mPointY=getArguments().getFloat("pointY");//レイアウトのy座標の設定
+		mWidth=getArguments().getInt("width");//レイアウトの幅の設定
+		mHeight=getArguments().getInt("height");//レイアウトの高さの設定
+		
+		//画像ファイルのサイズを変更する
+		resizeFig();
+		
+		//画像を登録する
 		ImageView bg = (ImageView) mView.findViewById(R.id.story_background);
-		bg.setImageResource(R.drawable.ic_launcher);
-
+		bg.setImageBitmap(mBgFig);
+		
 		return mView;
 	}
-
+	
+	/**
+	 * 画像ファイルのサイズを変更する
+	 */
+	public void resizeFig(){
+		if (mBgFig.getHeight() >= mBgFig.getWidth()) {
+			// 縦長の画像の場合
+			mDrownBG_w = mHeight * (mBgFig.getWidth() / mBgFig.getHeight());// 背景画像の幅
+			mDrownBG_h = mHeight;// 背景画像の高さ
+		} else {
+			// 横長の画像の場合
+			mDrownBG_w = mWidth;// 背景画像の幅
+			mDrownBG_h = mWidth * (mBgFig.getHeight() / mBgFig.getWidth()); // 背景画像の高さ
+		}
+		mBgFig=	Bitmap.createScaledBitmap(mBgFig,mDrownBG_w,mDrownBG_h,true);
+	}
+	
 	@Override
 	public void onStart() {
 		super.onStart();
-	}
-	 
-	
-
-	public Bitmap getBackgroundFile() {
-		return mBitmap;
-	}
-
-	public void setbackground(Bitmap mBitmap) {
-		this.mBitmap = mBitmap;
-	}
-	
-	public void settest(String a){
-		this.mm = a;
 	}
 }
