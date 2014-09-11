41dce82cfe649968b422c99a7d7e26f8987b718c
yuhattori <yuhattori14@gmail.com>
2014-09-11 16:47:21 +0900

    コメントの修正

--- src/com/illustratedbooks/fragment/StoryMessegeWindowFragment.java
+++ src/com/illustratedbooks/fragment/StoryMessegeWindowFragment.java
@@ -15,34 +15,36 @@ import android.widget.LinearLayout;
 
 public class StoryMessegeWindowFragment extends Fragment {
 	private View mView;
-	private String mText;
-	
-	/*レイアウトの情報*/
-	private float mPointX;//レイアウトのx座標
-	private float mPointY;//レイアウトのy座標
-	private int mWidth;//レイアウトの幅
-	private int mHeight;//レイアウトの高さ
-	
+	private String mText;// 表示するテキスト
+
+	/* レイアウトの情報 */
+	private float mPointX;// レイアウトのx座標
+	private float mPointY;// レイアウトのy座標
+	private int mWidth;// レイアウトの幅
+	private int mHeight;// レイアウトの高さ
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		mView =inflater.inflate(R.layout.fragment_story_messege_window, container, false);
-		
-		//Bundleから情報を取得する
-		mText=getArguments().getString("Text");//画像ファイルを設定
-		mPointX=getArguments().getFloat("pointX");//レイアウトのx座標の設定
-		mPointY=getArguments().getFloat("pointY");//レイアウトのy座標の設定
-		mWidth=getArguments().getInt("width");//レイアウトの幅の設定
-		mHeight=getArguments().getInt("height");//レイアウトの高さの設定
-		
-		LinearLayout layout = (LinearLayout) mView.findViewById(R.id.messege_window_linearlayout);
+		mView = inflater.inflate(R.layout.fragment_story_messege_window,
+				container, false);
+
+		// Bundleから情報を取得する
+		mText = getArguments().getString("Text");// 表示テキストを設定
+		mPointX = getArguments().getFloat("pointX");// レイアウトのx座標の設定
+		mPointY = getArguments().getFloat("pointY");// レイアウトのy座標の設定
+		mWidth = getArguments().getInt("width");// レイアウトの幅の設定
+		mHeight = getArguments().getInt("height");// レイアウトの高さの設定
+
+		LinearLayout layout = (LinearLayout) mView
+				.findViewById(R.id.messege_window_linearlayout);
 		MessegeSurfaceView sf = new MessegeSurfaceView(getActivity());
 		layout.addView(sf);
 		return mView;
 	}
-	
+
 	private class MessegeSurfaceView extends SurfaceView implements
-	SurfaceHolder.Callback{
+			SurfaceHolder.Callback {
 
 		public MessegeSurfaceView(Context context) {
 			super(context);
@@ -52,7 +54,7 @@ public class StoryMessegeWindowFragment extends Fragment {
 		@Override
 		public void surfaceCreated(SurfaceHolder holder) {
 			// TODO 自動生成されたメソッド・スタブ
-			
+
 		}
 
 		@Override
@@ -61,15 +63,15 @@ public class StoryMessegeWindowFragment extends Fragment {
 			// TODO 自動生成されたメソッド・スタブ
 			int a = width;
 			int b = height;
-			b=a;
-			a=b;
+			b = a;
+			a = b;
 		}
 
 		@Override
 		public void surfaceDestroyed(SurfaceHolder holder) {
 			// TODO 自動生成されたメソッド・スタブ
-			
+
 		}
-		
+
 	}
 }
